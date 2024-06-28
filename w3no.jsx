4ab71178966f7cb08fe24f35f0943616859abc66
boyuia <dodylan190@gmail.com>
2024-06-29 09:06:01 +1000

    Added React code

---
+++ w3no.jsx
@@ -0,0 +1,16 @@
+function MyButton() {
+  return (
+    <button>
+      Weno: Comming Soon
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Weno is comming soon, get ready to join the conversation</h1>
+      <MyButton />
+    </div>
+  );
+}
